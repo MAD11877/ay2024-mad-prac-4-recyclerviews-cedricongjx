654b8b53faf40a3f409c3507aa59bc2816afe53d
cedric.ongjx <cedric.ongjx@gmail.com>
2024-05-19 16:55:59 +0800

    please work please work

---
+++ app/src/main/java/sg/edu/np/mad/madpractical4/User.java
@@ -0,0 +1,41 @@
+package sg.edu.np.mad.madpractical4;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.content.Context;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class User implements Serializable {
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
+    public User(String name, String description, int id, boolean followed){
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.followed = followed;
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public boolean getFollowed() {
+        return followed;
+    }
+
+}
